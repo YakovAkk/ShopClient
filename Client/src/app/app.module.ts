84dea4d09e6d09b908e3068c875195606e6d4b08
Yakov <akovasko@gmail.com>
2022-02-18 15:21:44 +0200

    was fixed bags

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,26 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
     SideBarComponent,
-    HeaderComponent,
-    MainComponent
+    HeaderComponent
   ],
-  imports: [  
+  imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatSliderModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
